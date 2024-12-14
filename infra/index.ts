84d8711a58df1447c66bd8052774e7b16b527f57
Yash Bhavi <yashbhavi3@gmail.com>
2024-12-14 21:21:30 +0530

    infra changes

--- infra/index.ts
+++ infra/index.ts
@@ -1,18 +1,17 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
-const service = new gcp.cloudrun.Service("express-assignment", {
+const service = new gcp.cloudrun.Service("express-app", {
     location: "asia-south1",
     template: {
         spec: {
             containers: [{
-                image: "gcr.io/assignment-444606/express-assignment:latest",
+                image: "gcr.io/assignment-444606/express-app:v2",
             }],
         },
     },
 });
 
-const topic = new gcp.pubsub.Topic("rate-limit-events");
-
+// const topic = new gcp.pubsub.Topic("projects/assignment-444606/topics/rate-limit-event");
 export const url = service.statuses.apply(statuses => statuses[0].url);
 console.log('url: ', url);
